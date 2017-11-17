8536d8be19a0e4d71094d31655134f94e48e92c3
Tony Shannon <tony.shannon@frectal.com>
2017-11-17 16:09:40 +0000

    spotify change

--- Jamming2/jamming/src/util/Spotify.js
+++ Jamming2/jamming/src/util/Spotify.js
@@ -30,59 +30,60 @@ let Spotify = {
     }
   },
 
-  search(term){
-    const accessToken = Spotify.getAccessToken();
-    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
-          {
-            headers: {Authorization: `Bearer ${accessToken}`}
-          })
-    .then(response => response.json())
-    .then(jsonResponse => {
-      if (!jsonResponse.tracks){
-        return [];
-      }
-      return jsonResponse.tracks.items.map(track => ({
-        id: track.id,
-        name: track.name,
-        artist: track.artists[0].name,
-        album: track.album.name,
-        URI: track.uri
-      }));
-    });
-  },
 
-  savePlaylist(name, trackURIs){
-  if (name && trackURIs) {
-    const accessToken= Spotify.getAccessToken();
-    const headers=  {Authorization: `Bearer ${accessToken}`};
+  search(term) {
+     const accessToken = Spotify.getAccessToken();
+     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+       headers: {
+         Authorization: `Bearer ${accessToken}`
+       }
+     }).then(response => {
+       return response.json();
+     }).then(jsonResponse => {
+       if (!jsonResponse.tracks) {
+         return [];
+       }
+       return jsonResponse.tracks.items.map(track => ({
+         id: track.id,
+         name: track.name,
+         artist: track.artists[0].name,
+         album: track.album.name,
+         uri: track.uri
+       }));
+     });
+   },
+
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+    return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
     let userId;
 
-    return fetch('https://api.spotify.com/v1/me', {headers: headers})
-    .then(response => response.json())
-    .then(jsonResponse => {
-      const userId=jsonResponse.id;
-      return fetch (`https://api.spotify.com/v1/users/${userId}/playlists/`,
-      { headers: headers,
-        method: 'POST',
-        body: JSON.stringify({ name: name})
-      })
-    .then(response => response.json())
-    .then(jsonResponse => {
-      const playlistId = jsonResponse.id;
-      return fetch (`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+    return fetch('https://api.spotify.com/v1/me', {headers: headers}
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userId = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({name: name})
+      }).then(response => response.json()
+      ).then(jsonResponse => {
+        const playlistId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+            headers: headers,
+            method: 'POST',
+            body: JSON.stringify({uris: trackUris})
+          });
+        });
+      });
+    }
 
-      { 'Content-type': 'application/json',
-        method: 'POST',
-        headers: { Authorization: `Bearer ${accessToken}` },
-        //headers: headers,
-        //method: 'POST',
-        body: JSON.stringify({ uris: trackURIs })
-      }
-    );
-  });
-  });
-  } else {return;}
-} // end of savePlaylist
+
 }; // end of Spotity object
 
 export default Spotify;
